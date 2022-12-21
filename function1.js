dc95df6eb0bb74aa8c62735d4a1cef2b86f095c7
syamanthak <syamanthakr1000@gmail.com>
2022-12-21 19:55:45 +0530

    first change

---
+++ function1.js
@@ -0,0 +1,32 @@
+// function add (a,b)
+// {return a+b}
+
+// console.log(add(2,4))
+
+// function findFactorial(number) {
+//         if (number === 0 || number === 1)
+//             {
+//             return 1;
+//             }
+//         for (var i = number - 1; i >= 1; i--)
+//             { 
+//                 number = number * i;
+//             }
+//     return number;
+//   }
+// x = findFactorial(9)
+// console.log(x)
+
+// function frheat(a)
+// {
+//    b = (a * 9/5) + 32
+//    return b
+// }
+// function ceheat(a)
+// {
+//    d = (a - 32) * 5/9
+//    return d
+// }
+// console.log("The celcius value of given farenheat is",frheat(100))
+// console.log("The farenheat value of given celcius is",ceheat(200))
+
